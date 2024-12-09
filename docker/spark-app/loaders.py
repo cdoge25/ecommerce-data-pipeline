c779e4d407d5484004a1382a26521b737d7b63b7
cdoge25 <hainguyenducthien@gmail.com>
2024-12-09 08:52:30 +0700

    snowflake dw alternative

--- docker/spark-app/loaders.py
+++ docker/spark-app/loaders.py
@@ -11,8 +11,17 @@ MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
+POSTGRES_SCHEMA = os.getenv("POSTGRES_SCHEMA")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_WAREHOUSE = os.getenv("SNOWFLAKE_WAREHOUSE")
+SNOWFLAKE_DATABASE = os.getenv("SNOWFLAKE_DATABASE")
+SNOWFLAKE_SCHEMA = os.getenv("SNOWFLAKE_SCHEMA")
+SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_ROLE = os.getenv("SNOWFLAKE_ROLE")
 
 PROCESSED_FILE_PATH_LIST_STR = os.getenv("PROCESSED_FILE_PATH_LIST_STR")
 
@@ -51,10 +60,54 @@ def _load_to_postgres():
             "driver": "org.postgresql.Driver",
         }
         table_name = file_path.split("/")[-1].split(".")[0]
-        table = f"stg.{table_name}"
+        table = f"{POSTGRES_SCHEMA}.{table_name}"
         df.write.jdbc(url=url, table=table, mode="overwrite", properties=properties)
         print("=" * 100)
     os.system("kill %d" % os.getpid())
 
 
-_load_to_postgres()
+def _load_to_snowflake():
+    spark = (
+        SparkSession.builder.appName("LoadToSnowflake")
+        .config(
+            "spark.jars",
+            "spark/jars/spark-snowflake_2.12-2.12.0-spark_3.3.jar,spark/jars/snowflake-jdbc-3.13.30.jar,spark/jars/aws-java-sdk-bundle-1.11.1026.jar,spark/jars/hadoop-aws-3.3.2.jar",
+        )
+        .config("fs.s3a.access.key", MINIO_ACCESS_KEY_ID)
+        .config("fs.s3a.secret.key", MINIO_SECRET_ACCESS_KEY)
+        .config(
+            "fs.s3a.endpoint",
+            MINIO_ENDPOINT,
+        )
+        .config("fs.s3a.connection.ssl.enabled", "false")
+        .config("fs.s3a.path.style.access", "true")
+        .config("fs.s3a.attempts.maximum", "1")
+        .config("fs.s3a.connection.establish.timeout", "50")
+        .config("fs.s3a.connection.timeout", "100")
+        .getOrCreate()
+    )
+    processed_file_path_list = json.loads(
+        PROCESSED_FILE_PATH_LIST_STR.replace("'", '"')
+    )
+    for file_path in processed_file_path_list:
+        print(f"Reading: {file_path}")
+        df = spark.read.parquet(f"s3a://{file_path}")
+        sf_options = {
+            "sfURL": f"https://{SNOWFLAKE_ACCOUNT}.snowflakecomputing.com",
+            "sfWarehouse": SNOWFLAKE_WAREHOUSE,
+            "sfDatabase": SNOWFLAKE_DATABASE,
+            "sfSchema": SNOWFLAKE_SCHEMA,
+            "sfUser": SNOWFLAKE_USER,
+            "sfPassword": SNOWFLAKE_PASSWORD,
+            "sfRole": SNOWFLAKE_ROLE,
+        }
+        table_name = file_path.split("/")[-1].split(".")[0]
+        table = f"{SNOWFLAKE_SCHEMA}.{table_name}"
+        df.write.format("snowflake").options(**sf_options).option(
+            "dbtable", table
+        ).mode("overwrite").save()
+        print("=" * 100)
+    os.system("kill %d" % os.getpid())
+
+
+_load_to_snowflake()
